52916453fccb47d57fe982bd0e53770b29828a9e
Brock <45976315+brockcataldi@users.noreply.github.com>
2025-08-26 21:29:06 -0400

    Cleaned up a bunch of the boilerplate, added css variables, prettier, and started writing my blog post

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
+import { defineConfig } from "astro/config";
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
+import rehypeUnwrapImages from "rehype-unwrap-images";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://brockcataldi.com",
+  integrations: [mdx(), sitemap()],
+  markdown: {
+    rehypePlugins: [rehypeUnwrapImages],
+  },
 });
