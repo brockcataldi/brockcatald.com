b06ebdd73b2f12b84f5c26433657e7713f69cdf7
Brock <45976315+brockcataldi@users.noreply.github.com>
2025-08-30 21:30:34 -0400

    Adjusted the colors, tweaked more styling, addings logos and did some housekeeping

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,18 +1,27 @@
-import { defineCollection, z } from "astro:content";
-import { glob } from "astro/loaders";
+import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
 
-const blog = defineCollection({
-  // Load Markdown and MDX files in the `src/content/blog/` directory.
-  loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
-  // Type-check frontmatter using a schema
-  schema: ({ image }) =>
-    z.object({
-      title: z.string(),
-      description: z.string(),
-      // Transform string to Date object
-      publishDate: z.coerce.date(),
-      updatedDate: z.coerce.date().optional(),
-    }),
+const articles = defineCollection({
+	loader: glob({ base: './src/content/articles', pattern: '**/*.{md,mdx}' }),
+	schema: () =>
+		z.object({
+			title: z.string(),
+			description: z.string(),
+			publishDate: z.coerce.date(),
+			updatedDate: z.coerce.date().optional(),
+		}),
 });
 
-export const collections = { blog };
+const work = defineCollection({
+	loader: glob({ base: './src/content/work', pattern: '**/*.{md,mdx}' }),
+	// Type-check frontmatter using a schema
+	schema: () =>
+		z.object({
+			title: z.string(),
+			description: z.string(),
+			publishDate: z.coerce.date(),
+			updatedDate: z.coerce.date().optional(),
+		}),
+});
+
+export const collections = { articles };
